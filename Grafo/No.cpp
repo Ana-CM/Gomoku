38d2cf19be7d1a63a5d60abcd7581ffcb8ac23bc
Felippe Lobo <felippeloboabreu@gmail.com>
2022-02-10 01:41:00 -0300

    heuristica

--- Grafo/No.cpp
+++ Grafo/No.cpp
@@ -4,15 +4,15 @@ using namespace std;
 #include "No.h"
 #include "string"
 
-No::No(int idp, int idPaip, int colunap, int linhap, int jogadorp, Tabuleiro *tab)
+No::No(int idp, int idPaip, int jogadorp, Tabuleiro *tab)
 {
     id = idp;
     idPai = idPaip;
     tabuleiro = tab;
-    
-    tabuleiro->fazerJogada(colunap, linhap, jogadorp);
-    
-    //heuristica = calculaheuristica();
+    jogador = jogadorp;
+    heuristica = 0;
+    estado = tabuleiro->fazerJogada(jogadorp);
+    calculaHeuristica();
 };
 
 int No::getId()
@@ -20,7 +20,7 @@ int No::getId()
     return id;
 };
 
-Tabuleiro* No::getTabuleiro()
+Tabuleiro *No::getTabuleiro()
 {
     return tabuleiro;
 };
@@ -30,9 +30,14 @@ int No::getHeuristica()
     return heuristica;
 };
 
-string No::getEstado()
+int No::getEstado()
 {
     return estado;
+};
+
+int No::getJogador()
+{
+    return jogador;
 };
 
 void No::addFilho(int id)
@@ -40,4 +45,142 @@ void No::addFilho(int id)
     idsFilhos.push_back(id);
 };
 
-//int No:: calculaHeuristica();
+void No::calculaHeuristica()
+{
+
+    int contador = 0;
+    bool pcaBranca = true;
+
+    vector<vector<int>> tabuleiroTiraTeima;
+    tabuleiroTiraTeima.clear();
+
+    tabuleiroTiraTeima.resize(4);
+
+    for (int i = 0; i < 4; i++)
+    {
+        tabuleiroTiraTeima[i].resize(4);
+        for (size_t j = 0; j < 4; j++)
+        {
+            tabuleiroTiraTeima[i][j] = 0;
+        }
+    }
+
+    for (size_t i = 0; i < 4; i++)
+    {
+        for (size_t j = 0; j < 4; j++)
+        {
+            if (tabuleiro->verificarCasa(i, j) == 1 && tabuleiroTiraTeima[i][j] == 0)
+            {
+
+                if (i == j)
+                {
+
+                    for (int k = 0; k < 4; k++)
+                    {
+                        if (tabuleiro->verificarCasa(k, k) == 2)
+                        {
+                            tabuleiroTiraTeima[k][k] = 1;
+                            contador = 0;
+                            break;
+                        }
+
+                        if (tabuleiro->verificarCasa(k, k) == 1)
+                        {
+                            tabuleiroTiraTeima[k][k] = 1;
+                            contador += 3;
+                        }
+
+                        if (tabuleiro->verificarCasa(k, k) == 0)
+                        {
+                            tabuleiroTiraTeima[k][k] = 1;
+                            contador += 1;
+                        }
+                    }
+                    heuristica += contador;
+                    contador = 0;
+                }
+
+                if ((i == 0 && j == 3) || (i == 1 && j == 2) || (i == 2 && j == 1) || (i == 3 && j == 0))
+                {
+
+                    for (int w = 0; w < 4; w++)
+                    {
+                        for (int k = 3; k >= 0; k--)
+                        {
+                            if (tabuleiro->verificarCasa(w, k) == 2)
+                            {
+                                tabuleiroTiraTeima[w][k] = 1;
+                                contador = 0;
+                                break;
+                            }
+
+                            if (tabuleiro->verificarCasa(w, k) == 1)
+                            {
+                                tabuleiroTiraTeima[w][k] = 1;
+                                contador += 3;
+                            }
+
+                            if (tabuleiro->verificarCasa(w, k) == 0)
+                            {
+                                tabuleiroTiraTeima[w][k] = 1;
+                                contador += 1;
+                            }
+                        }
+                    }
+                    heuristica += contador;
+                    contador = 0;
+                }
+
+                for (size_t w = 0; w < 4; w++)
+                {
+                    if (tabuleiro->verificarCasa(i, w) == 2)
+                    {
+                        tabuleiroTiraTeima[i][w] = 1;
+                        contador = 0;
+                        break;
+                    }
+
+                    if (tabuleiro->verificarCasa(i, w) == 1)
+                    {
+                        tabuleiroTiraTeima[i][w] = 1;
+                        contador += 3;
+                    }
+
+                    if (tabuleiro->verificarCasa(i, w) == 0)
+                    {
+                        tabuleiroTiraTeima[i][w] = 1;
+                        contador += 1;
+                    }
+                }
+
+                heuristica += contador;
+                contador = 0;
+
+                for (size_t w = 0; w < 4; w++)
+                {
+                    if (tabuleiro->verificarCasa(w, j) == 2)
+                    {
+                        tabuleiroTiraTeima[w][j] = 1;
+                        contador = 0;
+                        break;
+                    }
+
+                    if (tabuleiro->verificarCasa(w, j) == 1)
+                    {
+                        tabuleiroTiraTeima[w][j] = 1;
+                        contador += 3;
+                    }
+
+                    if (tabuleiro->verificarCasa(w, j) == 0)
+                    {
+                        tabuleiroTiraTeima[w][j] = 1;
+                        contador += 1;
+                    }
+                }
+
+                heuristica += contador;
+                contador = 0;
+            }
+        }
+    }
+};
--- MainTeste.cpp
+++ MainTeste.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <cstdlib>
+#include <time.h>
 #include "./Grafo/Grafo.h"
 #include "./Grafo/No.h"
 #include "Tabuleiro.cpp"
@@ -10,11 +11,17 @@ using namespace std;
 int main()
 {
     Grafo *gomoku = new Grafo();
-    No *raiz = gomoku->getRaiz();
-    cout << raiz->getTabuleiro() << endl;
-    raiz->getTabuleiro()->fazerJogada(0,0,1);
-    raiz->getTabuleiro()->imprimeTabuleiro();
 
+    gomoku->setNo(2, 1);
+    gomoku->setNo(3, 2);
+    gomoku->setNo(4, 3);
+    gomoku->setNo(5, 4);
+    gomoku->setNo(6, 5);
+    gomoku->setNo(7, 6);
+    gomoku->setNo(8, 7);
+    gomoku->buscaNo(8)->getTabuleiro()->imprimeTabuleiro();
+    cout <<"Estado" <<  gomoku->buscaNo(8)->getEstado();
+    cout <<"Heuristica" << gomoku->buscaNo(8)->getHeuristica();
 
     return 0;
 }
